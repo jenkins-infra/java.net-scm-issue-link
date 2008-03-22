aecce44699a6b7b28297c4948fb16fc645ab2176
kohsuke <kohsuke@436c9196-4515-0410-abf8-e2a082904bc6>
2008-03-22 22:29:53 +0000

    relaxed a rule a bit.
    git-svn-id: https://www.dev.java.net/svn/cvs-news/trunk/cvs-news2/parser@105 436c9196-4515-0410-abf8-e2a082904bc6

--- src/com/sun/javanet/cvsnews/cli/UpdateCommand.java
+++ src/com/sun/javanet/cvsnews/cli/UpdateCommand.java
@@ -158,7 +158,7 @@ public class UpdateCommand extends AbstractIssueCommand {
     /**
      * Marked for marking bug as fixed.
      */
-    private static final Pattern FIXED = Pattern.compile("\\b\\[fixed\\]\\b");
+    private static final Pattern FIXED = Pattern.compile("\\b\\[.*(fixed|FIXED).*\\]\\b");
 
     // taken from http://fisheye5.cenqua.com/
     private static final Set<String> FISHEYE_CVS_PROJECT = new HashSet<String>(Arrays.asList(
